cd7768b19045fec979d7a7328b404235edd718b5
Rodbenz <84115603+Rodbenz@users.noreply.github.com>
2023-09-21 09:11:52 +0700

    comit

--- src/components/Skills/iconskills.js
+++ src/components/Skills/iconskills.js
@@ -33,33 +33,39 @@ export default function Iconskills() {
                     />
                 ))}
             </div>
-            <h1 className={"text-gray-400 text-[16px] md:text-[1rem] xl:text-[1.5rem] pb-2"}>
-                DATABAES
-            </h1>
-            <div className={"flex items-center space-x-8 pb-5"}>
-                {SKILLS.databases.map((el, index) => (
-                    <Image
-                        key={index}
-                        src={`/skills/${el}.svg`}
-                        width={'50'}
-                        height={'50'}
-                        alt={el}
-                    />
-                ))}
-            </div>
-            <h1 className={"text-gray-400 text-[16px] md:text-[1rem] xl:text-[1.5rem] pb-2"}>
-                OTHER
-            </h1>
-            <div className={"flex items-center space-x-8 pb-5"}>
-                {SKILLS.other.map((el, index) => (
-                    <Image
-                        key={index}
-                        src={`/skills/${el}.svg`}
-                        width={'50'}
-                        height={'50'}
-                        alt={el}
-                    />
-                ))}
+            <div className={"flex flex-row space-x-12 items-center"}>
+                <div>
+                    <h1 className={"text-gray-400 text-[16px] md:text-[1rem] xl:text-[1.5rem] pb-2"}>
+                        DATABAES
+                    </h1>
+                    <div className={"flex items-center space-x-8 pb-5"}>
+                        {SKILLS.databases.map((el, index) => (
+                            <Image
+                                key={index}
+                                src={`/skills/${el}.svg`}
+                                width={'50'}
+                                height={'50'}
+                                alt={el}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <div>
+                    <h1 className={"text-gray-400 text-[16px] md:text-[1rem] xl:text-[1.5rem] pb-2"}>
+                        OTHER
+                    </h1>
+                    <div className={"flex items-center space-x-8 pb-5"}>
+                        {SKILLS.other.map((el, index) => (
+                            <Image
+                                key={index}
+                                src={`/skills/${el}.svg`}
+                                width={'50'}
+                                height={'50'}
+                                alt={el}
+                            />
+                        ))}
+                    </div>
+                </div>
             </div>
         </>
     )
