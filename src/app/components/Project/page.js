7c9b714e5a443fa289670ba55663ea92c941a12a
Rodbenz <84115603+Rodbenz@users.noreply.github.com>
2023-10-06 13:16:26 +0700

    commit

--- src/app/components/Project/page.js
+++ src/app/components/Project/page.js
@@ -4,11 +4,11 @@ import ProjectCard from './ProjectCard'
 
 export default function Project() {
   return (
-    <section className={"w-full py-0"}>
-      {/* <div className={"flex pl-0 md:pl-10 xl:pl-32 pt-40"}>
+    <section id={MENULINKS[2].ref} className={"w-full min-h-screen py-0"}>
+      <div className={"flex pl-0 md:pl-10 xl:pl-32 pt-40"}>
         <h1 className={" text-fuchsia-700 text-4xl md:text-6xl xl:text-7xl font-bold"}>My Project</h1>
       </div>
-      <ProjectCard/> */}
+      <ProjectCard/>
     </section>
   )
 }
